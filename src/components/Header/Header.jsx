cd58419632b04f72617a22409781ecd1665e3644
tarnovskii <artem3dgame@gmail.com>
2023-07-05 04:42:52 +0300

    auth modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,17 @@ import s from './header.module.scss'
 
 import logo from 'media/img/logo.svg'
 import {useScrollDirection} from "../../hooks/useScrollDirection";
+import {AuthModal} from "../AuthModals/AuthModal";
+import {useCallback, useState} from "react";
 
 export const Header = () => {
     const scrollDirection = useScrollDirection()
+
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const signInButtonClickHandler = useCallback(() => {
+        setIsModalOpen(true)
+    }, [])
 
     return (
         <header data-hidden={scrollDirection === 'down'} className={s.header}>
@@ -13,8 +21,9 @@ export const Header = () => {
                 <a className={s.header__nav_link} href={'#section-easy-start'}>Invest</a>
                 <a className={s.header__nav_link} href={'#section-about'}>About</a>
                 <a className={s.header__nav_link} href={'#section-rewards'}>Rewards Program</a>
-                <button>Sign In</button>
+                <button onClick={signInButtonClickHandler}>Sign In</button>
             </nav>
+            <AuthModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
         </header>
     )
 }
