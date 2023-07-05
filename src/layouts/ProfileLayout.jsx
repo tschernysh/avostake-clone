9b8c4264ce843d176c9067725f224dc1d11533e1
tarnovskii <artem3dgame@gmail.com>
2023-07-06 00:11:18 +0300

    add new components

---
+++ src/layouts/ProfileLayout.jsx
@@ -0,0 +1,10 @@
+import {Outlet} from 'react-router-dom'
+
+export const ProfileLayout = () => {
+    return (
+        <main>
+            <h2>asdasdasd</h2>
+            <Outlet/>
+        </main>
+    )
+}
--- src/routes/routerSchema.jsx
+++ src/routes/routerSchema.jsx
@@ -1,6 +1,8 @@
 import { Navigate } from 'react-router-dom';
 import {MainLayout} from "../layouts/MainLayout/MainLayout";
 import {Landing} from "../components/Landing/Landing";
+import {ProfileLayout} from "../layouts/ProfileLayout";
+import {Dashboard} from "../components/Dashboard/Dashboard";
 
 export const routerSchema = [{
     path: '/',
@@ -10,4 +12,12 @@ export const routerSchema = [{
         index: true,
         element: <Landing/>,
     }],
+}, {
+    path: '/dashboard',
+    title: 'Dashboard',
+    element: <ProfileLayout/>,
+    children: [{
+        index: true,
+        element: <Dashboard/>
+    }]
 }];
