7a667d594c1af2ab80b00ba717a79bda4e21029c
tarnovskii <artem3dgame@gmail.com>
2023-07-06 21:31:26 +0300

    Updates

---
+++ static/js/760.8f6734d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkavostake_clone=self.webpackChunkavostake_clone||[]).push([[760],{17760:function(e,o,t){t.r(o);var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}}]);
+//# sourceMappingURL=760.8f6734d8.chunk.js.map
