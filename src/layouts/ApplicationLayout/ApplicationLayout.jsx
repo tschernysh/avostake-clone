bce27ad80be241060a41e48d0fd54b319156a28b
tschernysh <tschernysh@gmail.com>
2023-07-07 00:06:01 +0300

    commented redirect application

--- src/layouts/ApplicationLayout/ApplicationLayout.jsx
+++ src/layouts/ApplicationLayout/ApplicationLayout.jsx
@@ -1,19 +1,19 @@
 import { Outlet, useNavigate } from 'react-router-dom'
 import { ApplicationNavigation } from "../../components/ApplicationNavigation/ApplicationNavigation";
 import s from './application-layout.module.scss'
-import {ApplicationStatistics} from "../../components/ApplicationStatistics/ApplicationStatistics";
-import {ApplicationHeader} from "../../components/ApplicationHeader/ApplicationHeader";
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {ApplicationActionCreator} from "../../store/reducers/application/action-creator";
+import { ApplicationStatistics } from "../../components/ApplicationStatistics/ApplicationStatistics";
+import { ApplicationHeader } from "../../components/ApplicationHeader/ApplicationHeader";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ApplicationActionCreator } from "../../store/reducers/application/action-creator";
 
 export const ApplicationLayout = () => {
-    const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-    const { redirectTo } = useSelector(state => state.applicationReducer)
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-
+  const { redirectTo } = useSelector(state => state.applicationReducer)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  /*
     useEffect(() => {
         if (!!redirectTo) {
             const path = redirectTo
@@ -21,31 +21,31 @@ export const ApplicationLayout = () => {
             navigate(path)
         }
     }, [redirectTo])
+*/
+  useEffect(() => {
+    if (isNavOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
 
-    useEffect(() => {
-        if (isNavOpen) {
-            document.body.style.overflow = 'hidden'
-        } else {
-            document.body.style.overflow = 'auto'
-        }
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  }, [isNavOpen]);
 
-        return () => {
-            document.body.style.overflow = 'auto'
-        }
-    }, [isNavOpen]);
-
-    return (
-        <main className={s.app}>
-            <ApplicationNavigation isNavOpen={isNavOpen}/>
-            <section className={s.app__body}>
-                <ApplicationHeader setIsNavOpen={setIsNavOpen} isNavOpen={isNavOpen}/>
-                <div className={s.app__body__content_wrapper}>
-                    <div className={s.app__body__content_wrapper__content}>
-                        <Outlet/>
-                    </div>
-                    <ApplicationStatistics/>
-                </div>
-            </section>
-        </main>
-    )
+  return (
+    <main className={s.app}>
+      <ApplicationNavigation isNavOpen={isNavOpen} />
+      <section className={s.app__body}>
+        <ApplicationHeader setIsNavOpen={setIsNavOpen} isNavOpen={isNavOpen} />
+        <div className={s.app__body__content_wrapper}>
+          <div className={s.app__body__content_wrapper__content}>
+            <Outlet />
+          </div>
+          <ApplicationStatistics />
+        </div>
+      </section>
+    </main>
+  )
 }
